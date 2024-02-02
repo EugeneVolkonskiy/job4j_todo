bc963db0032322dd49959ab4db76c8eb08bebab3
EugeneVolkonskiy <3376361@mail.ru>
2024-02-02 17:19:43 +0300

    Модели

---
+++ src/main/java/ru/job4j/todo/model/Task.java
@@ -0,0 +1,25 @@
+package ru.job4j.todo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime created = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+    private boolean done;
+}
